2e7c1ed6d32eb6a9f1a5d4918e1c5f5da67a8030
Felipe Lohan <felipelohan03052006@gmail.com>
2024-12-06 16:04:37 -0300

    Adicionado estrutura do Switch case

--- src/App.java
+++ src/App.java
@@ -1,8 +1,30 @@
 import entities.*;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Product p = new Product();
-        
+        int opcao = 0;
+        Scanner teclado = new Scanner(System.in);
+
+        do{
+            System.out.println("------Sistema de pedido------");
+            System.out.println("1- Cadastrar cliente");
+            System.out.println("2- Cadastrar produto");
+            System.out.println("3- Adicionar produto ao carrinho");
+            System.out.println("4- Remover produto do carrinho");
+            System.out.println("5- Sair");
+
+            opcao = teclado.nextInt();
+                teclado.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }    
+        } while (opcao != 5);
     }
 }
